f66afdbe428e19060933c1cb6c187c4c847a8e8d
Maloy Baroi (XPL) <44752184+Maloy-Baroi@users.noreply.github.com>
2022-07-09 21:37:43 +0600

    9th July 2022 Maloy Baroi

---
+++ App_auth/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'App_auth/home.html')
+
+
+def login_or_signup(request):
+    return render(request, 'App_auth/login_or_signup.html')
+
+
+def login_signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(username=email, password=password)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('App_main:home'))
+        else:
+            createUser = User(username=email)
+            createUser.set_password(password)
+            createUser.save()
+            user = authenticate(username=email, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse('App_main:home'))
+
+    return HttpResponseRedirect(reverse('App_auth:login-signup'))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('App_auth:login-or-signup'))
+
